9a5a7f16bcff42a91ab3aea4a35ca6fee8009b40
rezanicgil <rezanicgil@gmail.com>
2024-02-24 02:21:01 +0300

    feat: routing

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -10,4 +11,12 @@ import {MatDividerModule} from '@angular/material/divider';
   standalone: true,
   imports: [MatCardModule, MatButtonModule, MatDividerModule],
 })
-export class CardsComponent {}
+export class CardsComponent {
+
+  constructor(private router: Router) {}
+  
+  openTale(){
+    this.router.navigate(['/tale']);
+  }
+
+}
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.css'
+  styleUrl: './slider.component.css',
 })
 export class SliderComponent {
+  constructor(private router: Router) {}
   images = [
     'https://i.pinimg.com/originals/81/17/14/8117143c41f688239197955b7016ebf4.jpg',
     'https://www.insaniyet.net/wp-content/uploads/2017/07/polygon-1920x1080-4k-hd-wallpaper-3d-241.jpg',
-    'https://i.pinimg.com/originals/c8/9e/87/c89e8708b8916fa7aba3c36dcd787526.jpg', 
+    'https://i.pinimg.com/originals/c8/9e/87/c89e8708b8916fa7aba3c36dcd787526.jpg',
   ];
 
+  goHomepage() {
+    this.router.navigate(['/']);
+  }
 }
