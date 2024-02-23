34899b9575ae4a487f66efafe67cb0b79601f21b
rezanicgil <rezanicgil@gmail.com>
2024-02-24 01:11:02 +0300

    style: general font and carousel text style

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -9,7 +9,7 @@ export class SliderComponent {
   images = [
     'https://i.pinimg.com/originals/81/17/14/8117143c41f688239197955b7016ebf4.jpg',
     'https://www.insaniyet.net/wp-content/uploads/2017/07/polygon-1920x1080-4k-hd-wallpaper-3d-241.jpg',
-    'https://i.pinimg.com/originals/81/17/14/8117143c41f688239197955b7016ebf4.jpg', 
+    'https://i.pinimg.com/originals/c8/9e/87/c89e8708b8916fa7aba3c36dcd787526.jpg', 
   ];
 
 }
