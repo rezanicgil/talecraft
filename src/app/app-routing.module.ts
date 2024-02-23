5fa9f15c424a671000eba67a910e71d4f2d48216
rezanicgil <rezanicgil@gmail.com>
2024-02-24 00:40:27 +0300

    feat: slider component has been created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SliderComponent } from './slider/slider.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SliderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.css'
+})
+export class SliderComponent {
+  images = [
+    'https://i.pinimg.com/originals/81/17/14/8117143c41f688239197955b7016ebf4.jpg',
+    'https://www.insaniyet.net/wp-content/uploads/2017/07/polygon-1920x1080-4k-hd-wallpaper-3d-241.jpg',
+    'https://i.pinimg.com/originals/81/17/14/8117143c41f688239197955b7016ebf4.jpg', 
+  ];
+
+}
